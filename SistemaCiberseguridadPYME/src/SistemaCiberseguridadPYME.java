5ff08edb4eb08fb6f366ea2e0a1815650e5e24e3
XFon12 <yxfon12@gmail.com>
2025-05-03 22:23:25 -0500

    feat: Clase GestorVulnerabilidades con sobrecarga de métodos

--- SistemaCiberseguridadPYME/src/SistemaCiberseguridadPYME.java
+++ SistemaCiberseguridadPYME/src/SistemaCiberseguridadPYME.java
@@ -2,34 +2,57 @@
 import java.util.Map;
 
 
-// Clase principal que demuestra todas las funcionalidades requeridas
+
 public class SistemaCiberseguridadPYME {
 
     public static void main(String[] args) {
         GestorVulnerabilidades gestor = new GestorVulnerabilidades();
-
-  
+        
+            // --- Sobrecarga de métodos ---
+            gestor.registrarVulnerabilidad("CVE-2023-1234"); 
+            gestor.registrarVulnerabilidad("Log4Shell", 10.0, "Vulnerabilidad crítica en Log4j"); 
 
         
     }
 }
 
-// Clase que gestiona vulnerabilidades (Sobrecarga de métodos + Errores)
+
 class GestorVulnerabilidades {
+    private Map<String, Vulnerabilidad> vulnerabilidades = new HashMap<>();
+    private PriorityQueue<Vulnerabilidad> colaCriticas = new PriorityQueue<>(
+            (v1, v2) -> Double.compare(v2.getCvss(), v1.getCvss())
+    );
+
+    //Sobrecarga de metodos
+    public void registrarVulnerabilidad(String cve) throws CVEInvalidoException {
+        if (!validarCVE(cve)) {
+            throw new CVEInvalidoException("Formato de CVE inválido. Use: CVE-AAAA-NNNN");
+        }
+        Vulnerabilidad vuln = new Vulnerabilidad(cve, 0.0, "Sin descripción");
+        vulnerabilidades.put(cve, vuln);
+    }    
+    public void registrarVulnerabilidad(String nombre, double cvss, String descripcion) {
+        if (cvss < 0.0 || cvss > 10.0) {
+            throw new IllegalArgumentException("CVSS debe ser entre 0.0 y 10.0");
+        }
+        String cve = "N/A-" + nombre.toUpperCase();
+        Vulnerabilidad vuln = new Vulnerabilidad(cve, cvss, descripcion);
+        vulnerabilidades.put(cve, vuln);
+        if (cvss >= 9.0) {
+            colaCriticas.add(vuln);  
+        }
+    }
     
 
-    
-
-    // Validación de CVE
     private boolean validarCVE(String cve) {
         return cve.matches("^CVE-\\d{4}-\\d{4,}$");
     }
 
-    // Muestra vulnerabilidades críticas (Colecciones)
+    
     
 }
 
-// Clase para representar una vulnerabilidad
+
 class Vulnerabilidad {
     private String cve;
     private double cvss;
@@ -41,7 +64,7 @@ class Vulnerabilidad {
         this.descripcion = descripcion;
     }
 
-    // Getters
+   
     public String getCve() { return cve; }
     public double getCvss() { return cvss; }
     public String getDescripcion() { return descripcion; }
